360c68ee5a71ac89759b6a4ca68f061e36999c2e
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-23 01:29:24 +0300

    добавил edit_list_get_vacancies для HeadHunterAPI

--- src/api_requests.py
+++ src/api_requests.py
@@ -33,6 +33,32 @@ class HeadHunterAPI(RequestsAPI):
         json_oobj = json.loads(data_req)
         return json_oobj
 
+    def edit_list_get_vacancies(self):
+        correct_list_vacancies = []
+        for i in self.get_vacancies()['items']:
+            vacancy_info = {}
+            vacancy_info['id'] = f"id: {i['id']}"
+            vacancy_info['employer'] = f"Компания: {i['employer']['name']}"
+            vacancy_info['profession'] = f" Должность: {i['name']}"
+            if i['salary'] == None:
+                vacancy_info['salary'] = 'Зарплата не указана'
+            elif i['salary']['from'] == None:
+                vacancy_info['salary'] = f"Зарплата: до {i['salary']['to']} {i['salary']['currency']}"
+            elif i['salary']['to'] == None:
+                vacancy_info['salary'] = f"Зарплата: от {i['salary']['from']} {i['salary']['currency']}"
+            else:
+                vacancy_info['salary'] = f"Зарплата: {i['salary']['from']}-{i['salary']['to']} {i['salary']['currency']}"
+            vacancy_info['town'] = f"Город: {i['area']['name']}"
+            vacancy_info['requirement'] = f"Требования: {i['snippet']['requirement']}"
+            vacancy_info['experience'] = f"Требуемый опыт: {i['experience']['name']}"
+            vacancy_info['employment'] = f"Занятость: {i['employment']['name']}"
+            vacancy_info['url'] = f"Ссылка на вакансию: https://www.hh.ru/vacancy/{vacancy_info['id'][4:]}"
+            vacancy_info['employment'] = f"Занятость: {i['employment']['name']}"
+
+            correct_list_vacancies.append(vacancy_info)
+        return correct_list_vacancies
+
+
     def print_info(self):
         """develop-метод, для отображения инфы по запросу"""
         for i in self.get_vacancies()['items']:
