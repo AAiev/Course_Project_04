fa0eb564029b66e661f134cab0e17959dc75c602
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-24 02:17:55 +0300

    Добавил родительский класс и перенес в него честь кода. pep8 разбил get_vacancies на get_request и get_vacancies

--- src/api_requests.py
+++ src/api_requests.py
@@ -2,108 +2,127 @@ import requests
 import json
 from abc import ABC, abstractmethod
 
-SUPERJOB_API_KEY = 'v3.r.137693076.8c0a621f62c1bc4c0fadc3056207d620545fa9c6.3a2d874af9ab61cef98ab134333dd3f98ecc486c'
-
 
 class RequestsAPI(ABC):
-    @abstractmethod
-    def __init__(self, keyword=''):
-        self.keyword = keyword
 
     @abstractmethod
-    def get_vacancies(self):
+    def get_request(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def print_info(self):
+    def get_vacancies(self, *args, **kwargs):
         pass
 
 
-class HeadHunterAPI(RequestsAPI):
-    def __init__(self, keyword):
-        self.keyword = 'NAME:' + keyword
-        # self.data_req = {}
+class ParsingError(Exception):
+    """Общий класс исключения для скриптов"""
 
-    def get_vacancies(self, keyword=''):
+    def __init__(self, *args):
+        self.message = args[0] if args else 'Ошибка получения вакансий.'
+
+    def __str__(self):
+        return self.message
+
+
+class ParentAPI(RequestsAPI):
+    def __init__(self):
+        self.keyword = ''
+        self.url = ''
+        self.param = []
+        self.headers = None
+        self.key_with_vacancies = ''
+
+    def get_request(self):
+        req = requests.get(self.url, self.params)
+        if req.status_code != 200:
+            raise ParsingError(f'Ошибка получения вакансий. Код статуса: {req.status_code}')
+        else:
+            return req
+
+    def get_vacancies(self, req):
         """Выдает вакансии по ключевому слову keyword"""
-        params = {'text': self.keyword}
-        req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
         data_req = req.content.decode()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
-        req.close()
-        json_oobj = json.loads(data_req)
-        return json_oobj
+        json_obj = json.loads(data_req)
+        return json_obj
 
-    def edit_list_get_vacancies(self):
+    def print_info(self, vacancy_list):
+        """develop-метод, для отображения инфы по запросу"""
+        if self.key_with_vacancies in vacancy_list:
+            for i in vacancy_list[self.key_with_vacancies]:
+                print(i)
+        else:
+            for i in vacancy_list:
+                print(i)
+
+
+class HeadHunterAPI(ParentAPI):
+    def __init__(self, keyword):
+        super().__init__()
+        self.keyword = keyword
+        self.params = {'text': self.keyword,
+                       "per_page": 10,
+                       "page": 0,
+                       }
+        self.url = 'https://api.hh.ru/vacancies'
+        self.key_with_vacancies = 'items'
+
+    def edit_list_get_vacancies(self, vacancy_list):
         correct_list_vacancies = []
-        for i in self.get_vacancies()['items']:
-            vacancy_info = {}
-            vacancy_info['id'] = f"id: {i['id']}"
-            vacancy_info['employer'] = f"Компания: {i['employer']['name']}"
-            vacancy_info['profession'] = f" Должность: {i['name']}"
-            if i['salary'] == None:
-                vacancy_info['salary'] = 'Зарплата не указана'
-            elif i['salary']['from'] == None:
-                vacancy_info['salary'] = f"Зарплата: до {i['salary']['to']} {i['salary']['currency']}"
-            elif i['salary']['to'] == None:
-                vacancy_info['salary'] = f"Зарплата: от {i['salary']['from']} {i['salary']['currency']}"
+        for i in vacancy_list[self.key_with_vacancies]:
+            vacancy_info = {'id': i['id'],
+                            'company': i['employer']['name'],
+                            'profession': i['name'],
+                            'salary_currency': i['salary']['currency'],
+                            'town': i['area']['name'],
+                            'requirement': i['snippet']['requirement'],
+                            'experience': i['experience']['name'],
+                            'employment': i['employment']['name']
+                            }
+            if i['salary'] is None:
+                vacancy_info['salary_from'] = None
+                vacancy_info['salary_to'] = None
             else:
-                vacancy_info['salary'] = f"Зарплата: {i['salary']['from']}-{i['salary']['to']} {i['salary']['currency']}"
-            vacancy_info['town'] = f"Город: {i['area']['name']}"
-            vacancy_info['requirement'] = f"Требования: {i['snippet']['requirement']}"
-            vacancy_info['experience'] = f"Требуемый опыт: {i['experience']['name']}"
-            vacancy_info['employment'] = f"Занятость: {i['employment']['name']}"
-            vacancy_info['url'] = f"Ссылка на вакансию: https://www.hh.ru/vacancy/{vacancy_info['id'][4:]}"
-
+                vacancy_info['salary_from'] = i['salary']['from']
+                vacancy_info['salary_to'] = i['salary']['to']
+            vacancy_info['url'] = f"https://www.hh.ru/vacancy/{vacancy_info['id'][4:]}"
             correct_list_vacancies.append(vacancy_info)
         return correct_list_vacancies
 
 
-    def print_info(self):
-        """develop-метод, для отображения инфы по запросу"""
-        for i in self.get_vacancies()['items']:
-            print(i)
-
-
-class SuperjobAPI(RequestsAPI):
-    headers = {'X-Api-App-Id': SUPERJOB_API_KEY}
+class SuperjobAPI(ParentAPI):
 
     def __init__(self, keyword):
+        super().__init__()
         self.keyword = keyword
+        self.params = {'count': 10,
+                       'keyword': self.keyword}
+        self.headers = {'X-Api-App-Id':
+                        'v3.r.137693076.8c0a621f62c1bc4c0fadc3056207d620545fa9c6.'
+                        '3a2d874af9ab61cef98ab134333dd3f98ecc486c'}
+        self.url = 'https://api.superjob.ru/2.0/vacancies'
+        self.key_with_vacancies = 'objects'
 
-    def get_vacancies(self, keyword=''):
-        """Выдает вакансии по ключевому слову keyword"""
-        req = requests.get('https://api.superjob.ru/2.0/vacancies/?' + 'srws=10&' + 'keys=' + self.keyword,
-                           headers=self.headers)
-        data_req = req.content.decode()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
-        req.close()
-        json_oobj = json.loads(data_req)
-        return json_oobj
+    def get_request(self):
+        req = requests.get(self.url, self.params, headers=self.headers)
+        if req.status_code != 200:
+            raise ParsingError(f'Ошибка получения вакансий. Код статуса: {req.status_code}')
+        else:
+            return req
 
-    def edit_list_get_vacancies(self):
+    def edit_list_get_vacancies(self, vacancy_list):
         correct_list_vacancies = []
-        for i in self.get_vacancies()['objects']:
-            vacancy_info = {}
-            vacancy_info['id'] = f"id: {i['id']}"
-            vacancy_info['employer'] = f"Компания: {i['firm_name']}"
-            vacancy_info['profession'] = f" Должность: {i['profession']}"
-            if i['payment_from'] == 0 and i['payment_to'] == 0:
-                vacancy_info['salary'] = 'Зарплата не указана'
-            elif i['payment_from'] == 0:
-                vacancy_info['salary'] = f"Зарплата: до {i['payment_to']} {i['currency']}"
-            elif i['payment_to'] == 0:
-                vacancy_info['salary'] = f"Зарплата: от {i['payment_from']} {i['currency']}"
-            else:
-                vacancy_info['salary'] = f"Зарплата: {i['payment_from']}-{i['payment_to']} {i['currency']}"
-            vacancy_info['town'] = f"Город: {i['town']['title']}"
-            vacancy_info['requirement'] = f"Требования: {i['candidat']}"
-            vacancy_info['experience'] = f"Требуемый опыт: {i['experience']['title']}"
-            vacancy_info['employment'] = f"Занятость: {i['type_of_work']['title']}"
-            vacancy_info['url'] = f"Ссылка на вакансию: {i['link']}"
-
+        for i in vacancy_list[self.key_with_vacancies]:
+            vacancy_info = {'id': i['id'],
+                            'company': i['firm_name'],
+                            'profession': i['profession'],
+                            'salary_from': i['payment_from'],
+                            'salary_to': i['payment_to'],
+                            'salary_currency': i['currency'],
+                            'town': i['town']['title'],
+                            'requirement': i['candidat'],
+                            'experience': i['experience']['title'],
+                            'employment': i['type_of_work']['title'],
+                            'url': i['link']
+                            }
             correct_list_vacancies.append(vacancy_info)
         return correct_list_vacancies
-
-    def print_info(self):
-        """develop-метод, для отображения инфы по запросу"""
-        for i in self.get_vacancies()['objects']:
-            print(i)
