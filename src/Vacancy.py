db16505b27fbef3aa43c1b754ab11bf2954cb0bb
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-25 22:01:19 +0300

    edit

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -5,14 +5,14 @@ class Vacancy:
         self.profession = dict_vacancy['profession']
         self.company = dict_vacancy['company']
         self.town = dict_vacancy['town']
-        if dict_vacancy['salary_from'] is not None and dict_vacancy['salary_to'] is not None:
-            self.salary = f"{dict_vacancy['salary_from']} - {dict_vacancy['salary_to']} {dict_vacancy['salary_currency']}"
+        if dict_vacancy['salary_from'] is None and dict_vacancy['salary_to'] is None:
+            self.salary = 'Не указана'
         elif dict_vacancy['salary_from'] is None:
             self.salary = f"до {dict_vacancy['salary_to']} {dict_vacancy['salary_currency']}"
         elif dict_vacancy['salary_to'] is None:
             self.salary = f"от {dict_vacancy['salary_from']} {dict_vacancy['salary_currency']}"
         else:
-            self.salary = 'Не указана'
+            self.salary = f"{dict_vacancy['salary_from']} - {dict_vacancy['salary_to']} {dict_vacancy['salary_currency']}"
         self.experience = dict_vacancy['experience']
         self.employment = dict_vacancy['employment']
         self.requirement = dict_vacancy['requirement']
