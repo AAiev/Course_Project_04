d2e8a7efc7ae521db0ca908b04df516691508ee5
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-25 22:05:06 +0300

    add def save_excel

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 from src.api_requests import HeadHunterAPI, SuperjobAPI
+import pandas as pd
+import os
 
 
 def get_result_choise_platform(num: int, user_keyword: str):
@@ -29,3 +31,9 @@ def get_result_choise_platform(num: int, user_keyword: str):
         correct_vacancies_sj = api_sj.edit_list_get_vacancies(vacancies_sj)
         correct_vacancies = correct_vacancies_hh + correct_vacancies_sj
         return correct_vacancies
+
+
+def save_excel(list_vac):
+    file_to_excel = pd.DataFrame.from_dict(list_vac)
+    path = os.path.join('data', 'list_vac.xlsx')
+    file_to_excel.to_excel(path)
