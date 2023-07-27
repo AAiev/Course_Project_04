47f81b702140f2a1d6d42afb38f52b34e00d27f1
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-28 02:02:18 +0300

    Корректировка: добавил в функцию add_vacancy цикл для перевода списка экземпляров в список словарей

--- src/json_saver.py
+++ src/json_saver.py
@@ -1,14 +1,20 @@
 import json
 import os
 from operator import itemgetter
+from pprint import pprint
 
 
 class JSONSaver:
     def __init__(self):
         self.filename = 'list_vacansies.json'
 
-    def add_vacancy(self, data_list):
+    def add_vacancy(self, list_instances_vacancies):
         """ Сохраняет (перезаписывает) полученный список вакансий в JSON-файл"""
+        data_list = []
+        for i in list_instances_vacancies:
+            dict_i = i.__dict__
+            data_list.append(dict_i)
+        pprint(data_list)
         path = os.path.join("data", self.filename)
         with open(path, 'w', encoding='utf-8') as file:
             json.dump(data_list, file, ensure_ascii=False, indent=4)
