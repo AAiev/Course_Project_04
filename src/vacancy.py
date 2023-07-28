012ebeedbd94f977cae8eed8fecbb87fb749afbd
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-07-28 03:24:59 +0300

    в vacancy.py: добавил в class Vacancy методы сравнения вакансий удалил ненужный код, после предыдущего редактирования

--- src/vacancy.py
+++ src/vacancy.py
@@ -9,15 +9,6 @@ class Vacancy:
         self.salary_to = dict_vacancy['salary_to']
         self.salary_currency = dict_vacancy['salary_currency']
         self.salary_mean = dict_vacancy['salary_mean']
-
-        # if dict_vacancy['salary_from'] is None and dict_vacancy['salary_to'] is None:
-        #     self.salary = 'Не указана'
-        # elif dict_vacancy['salary_from'] is None:
-        #     self.salary = f"до {dict_vacancy['salary_to']} {dict_vacancy['salary_currency']}"
-        # elif dict_vacancy['salary_to'] is None:
-        #     self.salary = f"от {dict_vacancy['salary_from']} {dict_vacancy['salary_currency']}"
-        # else:
-        #     self.salary = f"{dict_vacancy['salary_from']} - {dict_vacancy['salary_to']} {dict_vacancy['salary_currency']}"
         self.experience = dict_vacancy['experience']
         self.employment = dict_vacancy['employment']
         self.requirement = dict_vacancy['requirement']
@@ -37,3 +28,9 @@ class Vacancy:
                f'Зарплата: {self.salary}. {self.employment}.\n' \
                f'Опыт работы в должности: {self.experience}.' \
                f'{self.url}\n'
+
+    def __gt__(self, other):
+        return self.salary_mean > other.salary_mean
+
+    def __le__(self, other):
+        return self.salary_mean <= other.salary_mean
